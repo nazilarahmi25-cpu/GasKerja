c3fba2e4810f93d67e4136da5caacdda514522b7
Nazila Rahmi <nazilarahmi@gmail.com>
2026-05-12 12:07:45 -0700

    edit halaman welcome

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -225,7 +225,7 @@
 
     <div class="heroe">
 
-        <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+        <h1>WEBSITENYA GAS KERJA KICAUU</h1>
 
         <h2>The small framework with powerful features</h2>
 
