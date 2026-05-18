d8854de0375ad22dcca2c1ccdf60ab93ae3e7459
rennualifahrahma <renuifah0987@gmail.com>
2026-05-18 20:14:08 +0800

    new routes and home done ifha

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,46 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('landing_page');
+    }
+    public function about_us(): string
+    {
+        return view('about_us');
+    }
+    public function login(): string
+    {
+        return view('halaman_login');
+    }
+    public function register(): string
+    {
+        return view('halaman_register');
+    }
+    public function dashboard_pencari(): string
+    {
+        return view('dashboard_pencari');
+    }
+    public function dashboard_perusahaan(): string
+    {
+        return view('dashboard_perusahaan');
+    }
+    public function dashboard_admin(): string
+    {
+        return view('dashboard_admin');
+    }
+    public function detail_lowongan(): string
+    {
+        return view('detail_lowongan');
+    }
+    public function apply_lowongan(): string
+    {
+        return view('apply_lowongan');
+    }
+    public function profil(): string
+    {
+        return view('profil');
+    }
+    public function notifikasi(): string
+    {
+        return view('notifikasi');
     }
 }
