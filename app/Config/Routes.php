ea3f7b2ad1736bd1f077d3be7ddee308b5c3a648
Nazila Rahmi <nazilarahmi@gmail.com>
2026-05-19 12:09:37 -0700

    Update routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,25 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/home', 'Home::index');
 
-$routes->get('/about_us', 'Home::about_us');
+$routes->get('/', 'Home::index');
+
+$routes->get('/about-us', 'Home::about_us');
+
+$routes->get('/login', 'Home::login');
+
+$routes->get('/register', 'Home::register');
+
+$routes->get('/dashboard-pencari', 'Home::dashboard_pencari');
+
+$routes->get('/dashboard-perusahaan', 'Home::dashboard_perusahaan');
+
+$routes->get('/dashboard-admin', 'Home::dashboard_admin');
+
+$routes->get('/detail-lowongan', 'Home::detail_lowongan');
+
+$routes->get('/apply-lowongan', 'Home::apply_lowongan');
+
+$routes->get('/profil', 'Home::profil');
+
+$routes->get('/notifikasi', 'Home::notifikasi');
