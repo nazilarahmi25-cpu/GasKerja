72ce17c894936fbc8b63fc7947f79da598a7a3e2
luppy <sityramadhani.pata@gmail.com>
2026-05-19 15:00:56 +0800

    file views dan file public

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -20,6 +20,7 @@
         <nav class="menu">
             <a href="<?= base_url('/') ?>">Home</a>
             <a href="<?= base_url('/about-us') ?>">About Us</a>
+            <a href="<?= base_url('/lowongan') ?>">lowongan</a>
             <a href="<?= base_url('/login') ?>">Login</a>
             <a href="<?= base_url('/register') ?>" class="btn-primary">Daftar</a>
         </nav>
