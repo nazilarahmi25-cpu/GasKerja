52342911d3567153026e167824eeefda6aad48b0
nia nurmila <nianurmila689@gmail.com>
2026-06-08 09:12:17 -0700

    update

--- app/Database/Migrations/2026-05-25-033501_CreatPencariKerjaTable.php
+++ app/Database/Migrations/2026-05-25-033501_CreatPencariKerjaTable.php
@@ -64,7 +64,15 @@ class CreatePencariKerjaTable extends Migration
         ]);
 
         $this->forge->addKey('id', true);
-
+        
+        $this->forge->addForeignKey(
+            'user_id',
+            'users',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+        
         $this->forge->createTable('pencari_kerja');
     }
 
